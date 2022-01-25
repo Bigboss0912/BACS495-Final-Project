879949b0f8fe61f9ba38b57f2a25ec959946d5d9
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-01-24 22:31:13 -0700

    Update home.jsx

--- saenz-michael-final-project/src/components/home.jsx
+++ saenz-michael-final-project/src/components/home.jsx
@@ -8,7 +8,7 @@ function Home() {
           <div class="col-lg-7">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              src="../static/images/beach_sunset.jpg"
+              src={ require("../static/images/beach_sunset.jpg") }
               alt=""
             />
           </div>
