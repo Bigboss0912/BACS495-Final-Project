6643bc9bde57f3aed0651b9a15684723f40825b3
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-01-24 22:46:29 -0700

    Update navigation.jsx

--- saenz-michael-final-project/src/components/navigation.jsx
+++ saenz-michael-final-project/src/components/navigation.jsx
@@ -7,7 +7,7 @@ function Navigation() {
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="container">
           <NavLink className="navbar-brand" to="/">
-            React Multi-Page Website
+            Scientia Omnium
           </NavLink>
           <div>
             <ul className="navbar-nav ml-auto">
