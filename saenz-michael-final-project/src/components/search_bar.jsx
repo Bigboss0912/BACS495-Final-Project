e7abd7a4febe7bdff3d5f9056df6d3a8d763e8e2
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-01-26 12:25:42 -0700

    Update search_bar.jsx

--- saenz-michael-final-project/src/components/search_bar.jsx
+++ saenz-michael-final-project/src/components/search_bar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const SearchBar = ({ keyword, setKeyWord }) => {
-  const barStyling = { width:"20rem", background:"#F2F1F9", border:"none", padding:"0.5rem" };
+const SearchBar = ({ keyword, setKeyword }) => {
+  const barStyling = { width:"20rem", background:"#F2F1F9", border:"none", borderRadius:"33px", padding:"0.5rem" };
   return (
     <input
       style={barStyling}
       key="rand1"
       value={keyword}
       placeholder={"Search"}
-      onChange={ (e) => setKeyWord(e.target.value) }
+      onChange={ (e) => setKeyword(e.target.value) }
       />
   );
 }
