2437ae066faa6bbe0c80765e6253c279cf0fa82e
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-01-31 14:49:50 -0700

    Update index.js

--- saenz-michael-final-project/src/components/index.js
+++ saenz-michael-final-project/src/components/index.js
@@ -1,3 +1,4 @@
-export { default as Header } from "./header";
-export { default as Footer } from "./footer";
-export { default as Home } from "./home";
+export { default as Header } from './header';
+export { default as Footer } from './footer';
+export { default as Home } from './home';
+export { default as Register } from './register';
