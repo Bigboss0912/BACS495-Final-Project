d3a1ee9efb774645350ae1796b5b9caefa6e0458
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-01-31 14:49:58 -0700

    Update index.js

--- saenz-michael-final-project/src/index.js
+++ saenz-michael-final-project/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './static/css/index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {
   Header,
   Footer,
   Home,
-} from "./components";
+  Register,
+} from './components';
 
 ReactDOM.render(
   <Router>
     <Header />
     <Routes>
-      <Route path="/" element={ <Home /> } />
+      <Route path='/' element={ <Home /> } />,
+      <Route path='/register' element={ <Register /> } />
     </Routes>
     < Footer />
   </Router>,
