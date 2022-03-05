2d8a088b248f37d754c9196164cdf509cfef1052
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2022-03-04 20:41:45 -0700

    Update index.js

--- final-project/React-App-Front-End/src/components/index.js
+++ final-project/React-App-Front-End/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from './footer';
 export { default as Home } from './home';
 export { default as Register } from './register';
 export { default as App } from './testApp';
+export { default as Login } from './login';
